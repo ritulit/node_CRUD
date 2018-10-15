a9624bed02804d64847e01064b1ddd6277d9336c
ritulit <orit.lieber@gmail.com>
2018-10-15 12:27:38 +0300

    node CRUD homework final

--- app.js
+++ app.js
@@ -5,19 +5,16 @@ let fs = require("fs");
 let Person =require("./person.js");
 
 
-// Create express app:
 const app = express();
 
 
-// Use middlewares (app level - not controller level):
-// this middleware takes the content of the request`s body, and parses it to json format
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
-//check if "people.json" file exists
-//if not - create this file and init it with an empty array
+
 if (!fs.existsSync("people.json")) {
     fs.writeFileSync("people.json", "[]");
 }
@@ -47,7 +44,7 @@ app.get("/api/people", (req, res) => {
 app.delete("/api/people", (req, res) => {
     //delete a person by name in query param
     let peopleArr =JSON.parse(fs.readFileSync("./people.json"));
-    let filterPeopleArr = peopleArr.filter(element => element.name != req.query.name)
+    let filterPeopleArr = peopleArr.filter(element => element._name != req.query.name)
 
      if (filterPeopleArr.length < peopleArr.length) {
         
@@ -63,6 +60,51 @@ app.delete("/api/people", (req, res) => {
     }
 });
 
+app.post("/api/people", (req, res) => {
+
+    let peopleArr =JSON.parse(fs.readFileSync("./people.json"));
+    let newPerson = new Person.PersonClassPointer();
+    console.log(`${JSON.stringify(req.body)}`);
+
+    try {
+        for (key in req.body) {
+            newPerson[key] = req.body[key];
+        }
+
+        peopleArr.push(newPerson);
+
+        fs.writeFileSync("people.json", JSON.stringify(peopleArr));
+        res.status(201);
+        res.send("Person addedd to the file");
+    } catch (e) {
+        res.status(400);
+        res.send(e.message);
+    }
+});
+
+
+
+app.put("/api/people", (req, res) => {
+
+    let peopleArr =JSON.parse(fs.readFileSync("./people.json"));
+
+    let pointerToPerson = peopleArr.find(element => element._name == req.query.name);
+   
+    if (pointerToPerson) {
+        for (key in req.body) {
+            pointerToPerson["_"+key] = req.body[key];
+        }
+
+        //save the updates to the file
+        fs.writeFileSync("people.json", JSON.stringify(peopleArr));
+
+        res.status(200);
+        res.send("Person edited in the file");
+    } else {
+        res.status(400);
+        res.send("No such person in the file");
+    }
+});
 
 app.listen(4600, () => {
     console.log("server runs")
--- person.js
+++ person.js
@@ -1,41 +1,82 @@
+//let countries = require('./countries.json');
+let fs = require("fs");
+
+
 class Person {
 
     constructor() {
-        this.age;
-        this.name;
-        this.is_male;
-        this.country;
+        this._name;
+        this._age;
+        this._is_male;
+        this._country;
+        
             }
 
-    set age(value) {
-        let currentYear = new Date().getFullYear();
-        
-        if (value <= currentYear && value >= currentYear - 25)
-            this._year = value;
-        else
-            throw new Error(`Year must be between ${currentYear-25} to ${currentYear}`);
-    }
-    //should be between 0-120
-    set age(value) {
     
-       // if (value < 0 && value > 120)
-        //throw new Error(`Age must be between 0 to 120`);
+    //should be between 0-120
+    set age(value){
+        console.log(value);
+        if (parseInt(value) < 0 || parseInt(value) > 120)
+        throw new Error(`Age must be between 0 to 120`);
+        else
+        this._age=value;
           
       
+     }
+
+    get age(){
+        return this.age;
     }
 
     //should be 3-15 chars and unique
     set name(value){
+       
+        let indexOfName = JSON.parse(fs.readFileSync("./people.json")).findIndex(el => el._name == value);
+        console.log(indexOfName);
+       
+        if(value.length>=3 && value.length<=15 && indexOfName==-1)
+        this._name=value;
+        else{
+            if(value.length<3)
+            throw new Error(`Name length must be at least 3 chars`);
+            else if(value.length>15)
+            throw new Error(`Name length must be less than 15 chars`);
+            else
+            throw new Error(`Name must be unique`);
+        }
+        
+     }
 
-
+    get name(){
+        return this._name;
     }
 
     //should be boolean
-    set is_male(value){}
+    set is_male(value){
+        if(value=="true"){this._is_male=true;}
+        else
+        this._is_male=false;
+
+    }
+
+    get is_male(){
+        return this._is_male;
+    }
 
     //should be from the list
-    set country(value){}
+    set country(value){
+        let countryIndex = JSON.parse(fs.readFileSync("./countries.json")).findIndex(el => el == value);
+        if(countryIndex!=-1)
+        this._country=value;
+        else
+        throw new Error(`The country doesnt exist in the countries list`);
 
+    }
+
+    get country(){
+        return this._country;
+    }
+   
 
     
 
